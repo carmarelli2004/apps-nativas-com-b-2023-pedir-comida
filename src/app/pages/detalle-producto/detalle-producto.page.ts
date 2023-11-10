fb082002d46405843d733c1b9e54f07d7be83b00
Carmen <marellicarmen@gmail.com>
2023-11-10 12:44:56 -0300

    cambios 1

--- src/app/pages/detalle-producto/detalle-producto.page.ts
+++ src/app/pages/detalle-producto/detalle-producto.page.ts
@@ -14,11 +14,7 @@ export class DetalleProductoPage  {
   constructor(
     private activatedRoute: ActivatedRoute,
     public cartService: CartService
-<<<<<<< HEAD
     ) { 
-=======
-    ) {
->>>>>>> fa844422bc45f8a9873fee8096f81be19eb63fec
     activatedRoute.params.subscribe(params => {
       this.buscarProducto(params['nombre']);
     })
@@ -38,24 +34,6 @@ cantidad=1;
       }
     }
     }
-
-
-
-  cantidad = 1;
-
-  buscarProducto(nombreProductoABuscar:string){
-    for (let i = 0; i < CATEGORIAS.length; i++) {
-      const categoriaActual = CATEGORIAS[i];
-      for (let j = 0; j < categoriaActual.productos.length; j++) {
-        const productoActual = categoriaActual.productos[j];
-        if(productoActual.nombre === nombreProductoABuscar){
-          this.producto = productoActual;
-        }
-        //console.log(productoActual.nombre, nombreProductoABuscar, productoActual.nombre === nombreProductoABuscar)
-      }
-    }
-  }
-
 
   producto:Producto = {
     imagen: 'dsgdfbgs',
